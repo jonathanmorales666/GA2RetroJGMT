12bc0be6eebe28c6cc00ec8641932cb66bff1cc8
Jonathan Morales <jonathanmorales@emilianisomascos.edu.gt>
2026-04-14 08:02:59 -0600

    Ejercicio #6 terminado

--- EJ6_JGMT.py
+++ EJ6_JGMT.py
@@ -1,3 +1,7 @@
 # Retroalimentación GA2
 # Jonathan Gabriel Morales Torres "10"
-#
+# Input #3
+
+n = int(input("Introduce un número entero: "))
+suma = n* (n + 1 )/2
+print("La suma de los primeros enteros desde 1 hasta " + str(n) + " es " + str(suma))
