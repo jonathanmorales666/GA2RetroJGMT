bd120c4343f3da5393237d59fd4b36093824c084
Jonathan Morales <jonathanmorales@emilianisomascos.edu.gt>
2026-04-14 08:10:27 -0600

    Decimo y penultimo ejercicio terminado

--- EJ10_JGMT.py
+++ EJ10_JGMT.py
@@ -1,3 +1,12 @@
 # Retroalimentación GA2
 # Jonathan Gabriel Morales Torres "10"
-#
+# inversión
+
+inversión = float(input("Introduce la inversión inicial:  "))
+interes =0.04
+balance1 = inversión * (interes +1)
+print("Balance tras el primer año:" + str(round(balance1, 2)))
+balance2 = balance1 * (1 + interes)
+print("Balance tras el segundo año:" + str(round(balance2, 2)))
+balance3 = balance2 * (1 + interes)
+print("Balance tras el tercer año:" + str(round(balance3, 2)))
