216059b85713032ed4b7c438275b9aaefbe90efd
Jonathan Morales <jonathanmorales@emilianisomascos.edu.gt>
2026-04-14 08:07:10 -0600

    Ejercicio #8 terminadop

--- EJ8_JGMT.py
+++ EJ8_JGMT.py
@@ -1,3 +1,8 @@
 # Retroalimentación GA2
 # Jonathan Gabriel Morales Torres "10"
-#
+# Input
+
+cantidad = float(input("¿Cantidad a invertir? "))
+interes = float(input("¿Interés porcentual anual? "))
+años = int(input("¿Años?"))
+print("Capital final: " + str(round(cantidad * (interes / 100 + 1) ** años, 2)))
