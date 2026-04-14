90a309270a25a5a48d9d159d3f9bd227e1127b1e
Jonathan Morales <jonathanmorales@emilianisomascos.edu.gt>
2026-04-14 08:05:20 -0600

    Ejercicio #7 terminado

--- EJ7_JGMT.py
+++ EJ7_JGMT.py
@@ -1,3 +1,10 @@
 # Retroalimentación GA2
 # Jonathan Gabriel Morales Torres "10"
-#
+# imc
+
+peso = input("¿Cuál es tu peso en kg?: ")
+estatura = input("¿Cuál es tu estatura en metros?: ")
+
+imc = round(float(peso)/float(estatura)**2,2)
+
+print("Tu índice de masa corporal es de: ", str(imc))
