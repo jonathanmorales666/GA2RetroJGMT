4ed48b4e8de1128a868b6b99d63a746296355992
Jonathan Morales <jonathanmorales@emilianisomascos.edu.gt>
2026-04-14 08:00:18 -0600

    Ejercicio #5 terminado

--- EJ5_JGMT.py
+++ EJ5_JGMT.py
@@ -1,3 +1,9 @@
 # Retroalimentación GA2
 # Jonathan Gabriel Morales Torres "10"
-#
+# input 2
+
+horas = float(input("Introduce tus horas de trabajo: "))
+coste = float(input("Introduce lo que cobras por hora: "))
+
+paga = horas * coste
+print("Tu paga es de: ", paga)
